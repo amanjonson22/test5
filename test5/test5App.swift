b596c58f537425d5a025020c0b5bc3c61b88f617
amanda <amandacsr10@gmail.com>
2025-02-05 15:49:08 -0300

    Initial Commit

---
+++ test5/test5App.swift
@@ -0,0 +1,17 @@
+//
+//  test5App.swift
+//  test5
+//
+//  Created by AMANDA CAROLINE DA SILVA RODRIGUES on 05/02/25.
+//
+
+import SwiftUI
+
+@main
+struct test5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
